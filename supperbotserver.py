683bff474cacb8ad97d733d090359115787fbd9f
Jeremy Yew <Macintosh@Fors-MacBook-Pro.local>
2017-05-28 15:41:33 +0800

    refactored send_message v1

--- supperbotserver.py
+++ supperbotserver.py
@@ -1,5 +1,3 @@
-#git add . git commit -m "commit" git push heroku master
-
 from flask import Flask, request
 import json
 import requests
@@ -27,7 +25,7 @@ def handle_messages():
   print payload
   for sender, message in messaging_events(payload):
     print "Incoming from %s: %s" % (sender, message)
-    send_message(PAT, sender, message)
+    send_message(sender, message)
   return "ok"
 
 def messaging_events(payload):
@@ -38,20 +36,22 @@ def messaging_events(payload):
   messaging_events = data["entry"][0]["messaging"]
   for event in messaging_events:
     if "message" in event and "text" in event["message"]:
-      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+      yield event["sender"]["id"], {
+        "text": event["message"]["text"].encode('unicode_escape')}
     else:
-      yield event["sender"]["id"], "I can't echo this"
+      yield event["sender"]["id"], {
+        "text": "whatchu sayin fam??"}
 
 
-def send_message(token, recipient, text):
+def send_message(recipient, message):
   """Send the message text to recipient with id recipient.
   """
 
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
+    params={"access_token": PAT},
     data=json.dumps({
       "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
+      "message": message
     }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
@@ -59,3 +59,27 @@ def send_message(token, recipient, text):
 
 if __name__ == '__main__':
   app.run()
+
+
+
+# To-do:
+  # Refactor send messages
+  # Functionality: get started button, greeting text, structured messages, instant reply, loading, call by name, share bot, wit.ai, subscribe to notifications
+  # Plan features
+  # Implement all features
+  # Send for review
+  # Get feedback
+  # other ideas: ync general info, buttery orders, memes/comics
+
+#GIT
+  # git add .
+  # git commit -m "commit"
+  # git push heroku master hi
+
+#TESTING WITH SHELL SCRIPT
+  # create test.sh.
+  # Add permission: chmod +x ./test.sh (https://askubuntu.com/questions/38661/how-do-i-run-sh-files).
+  # Run python server file: $ python supperbotserver.py
+  # ./test.sh
+  # Unsubscribe to page to ensure messages dont get sent to heroku while testing mode.
+
