05f732d645297c98bcc1142f64409250782dff86
Jeremy Yew <yewern@gmail.com>
2017-07-10 16:22:18 +0800

    Refactor menu scraper

--- dh_menu_scrape.py
+++ dh_menu_scrape.py
@@ -3,19 +3,21 @@ import requests
 
 url = "https://studentlife.yale-nus.edu.sg/dining-experience/daily-dining-menu/"
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
-page = requests.get(url, headers=headers)
-tree = html.fromstring(page.content)
 
-def get_day_special (day, meal):
+
+def scrape(day, meal):
+
+    page = requests.get(url, headers=headers)
+    tree = html.fromstring(page.content)
     tab = ""
     special = ""
+
     if day == "friday":
         tab = "tab5"
     elif day == "today":
         tab = "tab_na"
-
-    if meal == "bf":
-        special = "Live Station"
+    if meal == "breakfast":
+        heading = "Live Station"
 
     xpath_string = ('//*[@id="%s"]'
     '//p/strong/u[contains(text(), "%s")]'
@@ -32,14 +34,11 @@ def get_day_special (day, meal):
     'preceding-sibling::p/strong/u)'
     '+ 1'
     ']'
-    '/text()' % (tab, special, tab, special))
+    '/text()' % (tab, heading, tab, heading))
 
-    day_special = tree.xpath(xpath_string)
+    items = tree.xpath(xpath_string)
 
-    if u'\xa0' in day_special:
-        day_special.remove(u'\xa0')
-
-    return day_special
-
-print get_day_special ("friday", "bf")
-
+    if u'\xa0' in items:
+        items.remove(u'\xa0')
+    print items
+    return items
