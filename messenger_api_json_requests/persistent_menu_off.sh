e34ec898918d6295d5c74add00cbaa2e9d7a6830
Jeremy Yew <yewern@gmail.com>
2017-08-02 18:00:21 +0800

    Create persistent_menu_off.sh

---
+++ messenger_api_json_requests/persistent_menu_off.sh
@@ -0,0 +1,5 @@
+curl -X DELETE -H "Content-Type: application/json" -d '{
+  "fields":[
+    "persistent_menu"
+  ]
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAZAygcjNS3sBAEZCWHjMDwU8gW0OartsOxT1MElrwMpB4mHZCuniZBifZAKIT3sPTYgfJNVzPfO0EMnZANwZBEGGYPMU6tStXMUDvZBIoNUXzxQ9aKOf7k33wTDATnWTn6B90mr5Ulvp27DTvbKqK75ER17GLLt6rX9XgPnNAltgwZDZD"
