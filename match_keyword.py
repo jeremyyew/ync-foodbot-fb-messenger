98d2668a2f32bd3c82017df399a0f4f20f65a95d
Jeremy Yew <yewern@gmail.com>
2017-08-03 14:38:40 +0800

    Move #feedback in front of feedback

--- match_keyword.py
+++ match_keyword.py
@@ -62,14 +62,12 @@ def match_text_or_payload(input, sender_id):
     elif input_contains(["help", "HELP_PB"]):
         yield msg.start_msg
 
-    elif input_contains(["FEEDBACK_PB", "feedback"]):
-        yield msg.feedback_prompt_msg
-
-
     elif input_contains(["#feedback"]):
         yield msg.feedback_received_msg
 
 
+    elif input_contains(["FEEDBACK_PB", "feedback"]):
+        yield msg.feedback_prompt_msg
 
 
     #elif input_contains(["EXPLORE_REVIEWS_PB"]):
