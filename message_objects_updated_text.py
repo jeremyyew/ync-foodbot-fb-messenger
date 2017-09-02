8b1cb371c271ca021e8f6ccc389a416bbcee4ca5
jeremyyew <yewern@gmail.com>
2017-09-02 15:22:07 +0800

    Separated text for repeated updating

---
+++ message_objects_updated_text.py
@@ -0,0 +1,20 @@
+welcome_msg_text = "Welcome to the Yale-NUS Foodbot! " \
+                   "Feel free to share this if you find it useful. Please do send feedback or suggestions" + u'\U0001f60a'
+
+share_msg_text = "Hungry? I gotchu fam."
+
+share_msg_subtitle_text = "Quick access to dining hall mealtimes, buttery opening hours, delivery menus/hotlines, and more!"
+
+dh_info_msg_text = u'\U0001f374 ' + "Dining hall mealtimes:\n" \
+                           "-Mon-Fri: 7.30-9.30am, 11.30am-1.30pm, 6-8.30pm\n" \
+                           "-Sat-Sun: 10am-1pm, 6-8.30pm\n" \
+                           "-Green & Healthy Lunch: Mon (Elm), Wed (Cen), Fri (Saga)\n"
+
+buttery_msg_text = u'\U0001f354 ' + "Buttery openings:  \n " \
+                                "-The Nest: TBC\n" \
+                                "-Shiner's Diner: Fri/Sun/Mon 930pm-1145mn\n" \
+                                "-Shiok Shack: Tue/Wed/Thu 9pm-12mn\n" \
+                                "(Subject to change, updated 2/9)"
+
+
+agora_msg_text = u'\u2615 ' + "Agora Opening Hours: Mon-Fri 8am-6pm, Sat 9am-3pm\nGrab & Go Hours: TBC\n"
