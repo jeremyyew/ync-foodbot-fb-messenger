4d8669a7e3f0e865518b37953c06b34e570d9e1c
jeremyyew <yewern@gmail.com>
2017-09-02 15:22:27 +0800

    Added a test_all arg

--- test_scripts/instant_test.py
+++ test_scripts/instant_test.py
@@ -2,15 +2,41 @@ import sys
 import requests, json
 
 test_text = sys.argv[1]
-payload = {"object": "page", "entry": [{"id": "979027202238929", "time": 1494428871668, "messaging": [
-            {"sender": {"id": "1484887258198358"}, "recipient": {"id": "979027202238929"},
-             "timestamp": 1494428871158, "message": {"mid": "mid.$cAAN6a6Ui58ZiJCwx9lb8ubp5XBDz", "seq": 52321,
-                                                     "text": test_text}}]}]}
+
 url = "http://127.0.0.1:5000/"
 headers = {'content-type': 'application/json'}
-r = requests.post(url, data=json.dumps(payload), headers=headers)
-print r.text
-if r.status_code != requests.codes.ok:
+
+if test_text == "test_all":
+    text_list = ["Get Started",
+                 "dh",
+                 "buttery",
+                 "amaan",
+                 "macs",
+                 "agora",
+                 "utown",
+                 "get all",
+                 "share",
+                 "help",
+                 "feedback",
+                 "#feedback"]
+    for text in text_list:
+        payload = {"object": "page", "entry": [{"id": "979027202238929", "time": 1494428871668, "messaging": [
+            {"sender": {"id": "1484887258198358"}, "recipient": {"id": "979027202238929"},
+             "timestamp": 1494428871158, "message": {"mid": "mid.$cAAN6a6Ui58ZiJCwx9lb8ubp5XBDz", "seq": 52321,
+                                                     "text": text}}]}]}
+        r = requests.post(url, data=json.dumps(payload), headers=headers)
+        print r.text
+        if r.status_code != requests.codes.ok:
+            print r.text
+
+else:
+    payload = {"object": "page", "entry": [{"id": "979027202238929", "time": 1494428871668, "messaging": [
+                {"sender": {"id": "1484887258198358"}, "recipient": {"id": "979027202238929"},
+                 "timestamp": 1494428871158, "message": {"mid": "mid.$cAAN6a6Ui58ZiJCwx9lb8ubp5XBDz", "seq": 52321,
+                                                         "text": test_text}}]}]}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
     print r.text
+    if r.status_code != requests.codes.ok:
+        print r.text
 
 
